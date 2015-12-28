0d31b705374c8a7ef4deee432f9784f5c0bebad7
Jap <jap@branchonline.nl>
2015-12-28 09:48:26 +0100

    Update branch-elements.js

--- branch-elements.js
+++ branch-elements.js
@@ -1,3 +1,5 @@
 module.exports.Form = require('./components/Form');
 module.exports.FormGroup = require('./components/FormGroup');
+module.exports.RowCollection = require('./components/RowCollection');
+module.exports.SortableList = require('./components/SortableList');
 module.exports.Tab = require('./components/Tab');
