81e500b7b98625939bc2ad50493562f16b5e1aa6
Jap <jap@branchonline.nl>
2015-12-28 11:38:37 +0100

    Update Tab.js

--- components/Tab.js
+++ components/Tab.js
@@ -10,6 +10,9 @@ module.exports = React.createClass({
     tabSelected: function(e) {
         this.setState({ activeTab: Number(e.target.dataset.id) });
     },
+    componentWillReceiveProps: function(nextProps) {
+        this.setState(nextProps);
+    },
     render: function() {
         var self = this;
 
@@ -18,17 +21,31 @@ module.exports = React.createClass({
 
         Object.keys(this.state.tabs).forEach(function(key, i) {
             var className = (i === self.state.activeTab) ? 'active' : '';
-            tabNames.push(React.createElement('li', {key: i + 'li', 'data-id': i, className: className, onClick: self.tabSelected}, key));
-            tabs.push(React.createElement('div', {key: i + 'tab', className: 'tab-container__content ' + className}, self.state.tabs[key]));
+            tabNames.push(
+                React.createElement(
+                    'li',
+                    {key: i + 'li', 'data-id': i, className: className, onClick: self.tabSelected},
+                    key
+                )
+            );
+            tabs.push(
+                React.createElement(
+                    'div',
+                    {key: i + 'tab', className: 'tab-container__content ' + className},
+                    self.state.tabs[key]
+                )
+            );
         });
 
         return React.createElement(
             'div',
             {className: 'tab-container'},
-            [
-                React.createElement('ul', {className: 'tab-container__tabs'}, tabNames),
-                tabs
-            ]
+            React.createElement(
+                'ul',
+                {className: 'tab-container__tabs'},
+                tabNames
+            ),
+            tabs
         );
     }
 });
